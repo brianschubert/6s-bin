b0fce778eed64a86576256dca564209e0321d319
Brian Schubert <schubert.b@northeastern.edu>
2023-08-07 13:57:02 -0400

    Add test for retrieving binary path

--- test/test_6s.py
+++ test/test_6s.py
@@ -3,6 +3,12 @@ import pytest
 import sixs_bin
 
 
+def test_get_path() -> None:
+    for version in sixs_bin._SIXS_BINARIES.keys():
+        binary = sixs_bin.get_path(version)
+        assert binary.is_file()
+
+
 def test_wrapper() -> None:
     _ = pytest.importorskip("Py6S")
     sixs_bin.test_wrappers()
