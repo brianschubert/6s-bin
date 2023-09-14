391a946135b1d21f30deae2cd7e65e8b905edd63
Brian Schubert <schubert.b@northeastern.edu>
2023-09-14 17:29:25 -0400

    Try -static-libgfortran instead of -static

--- build.py
+++ build.py
@@ -236,7 +236,7 @@ def build(target: SixSTarget, build_dir: pathlib.Path) -> None:
                 "make",
                 "-j",
                 "sixs",
-                "FC=gfortran -std=legacy -ffixed-line-length-none -ffpe-summary=none -static $(FFLAGS)",
+                "FC=gfortran -std=legacy -ffixed-line-length-none -ffpe-summary=none -static-libgfortran $(FFLAGS)",
             ],
             cwd=src_dir,
             capture_output=True,
