caaf3f4f05268532dcd9cea4b97d6a348802b0a5
Brian Schubert <schubert.b@northeastern.edu>
2023-09-16 12:11:57 -0400

    Migrate build script output to use logging and add log file option

--- build.py
+++ build.py
@@ -1,10 +1,7 @@
 """
 Build script.
 
-Downloads and builds 6S in a temporary directory, and it installs as a package
-resource.
-
-https://py6s.readthedocs.io/en/latest/installation.html#installing-6s
+Builds 6S in a temporary directory and it installs as a package resource.
 """
 # Copyright (C) 2023 Brian Schubert.
 #
@@ -27,18 +24,20 @@ import hashlib
 import http
 import http.client
 import io
+import logging
+import logging.config
 import os
 import pathlib
 import shutil
 import stat
 import subprocess
+import sys
 import tarfile
 import tempfile
-import textwrap
 import urllib.parse
 import urllib.request
 from dataclasses import dataclass
-from typing import Final, NamedTuple
+from typing import Any, Final, NamedTuple
 
 # Build script is always invoked from the base directory of the current distribution.
 DISTRIBUTION_ROOT: Final = pathlib.Path.cwd()
@@ -73,6 +72,7 @@ class SixSTarget(NamedTuple):
 class BuildConfig:
     archive_dir: pathlib.Path | None = None
     static_libgfortran: bool = False
+    log_file: pathlib.Path | None = None
 
     @classmethod
     def from_env(cls) -> BuildConfig:
@@ -82,10 +82,27 @@ class BuildConfig:
 
         static_libgfortran = os.environ.get("SIXS_STATIC_LIBGFORTRAN") == "1"
 
+        log_file = os.environ.get("SIXS_BUILD_LOG")
+        if log_file is not None:
+            log_file = pathlib.Path(log_file)
+
         return cls(
             archive_dir=archive_dir,
             static_libgfortran=static_libgfortran,
+            log_file=log_file,
         )
+
+
+class WrapFormatter(logging.Formatter):
+    indent: str
+
+    def __init__(self, *args: Any, indent=".. ", **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self._indent = f"\n{indent}"
+
+    def format(self, record: logging.LogRecord) -> str:
+        entry = super().format(record)
+        return entry.replace("\n", self._indent)
 
 
 TARGETS: Final = [
@@ -114,12 +131,15 @@ def _assert_detect_command(cmd: list[str]) -> None:
 
     Used to validate that a system dependency is installed and working correctly.
     """
+    logger = logging.getLogger(__name__)
+
     prog = cmd[0]
     try:
+        logger.debug(f"running {cmd}")
         result = subprocess.run(cmd, capture_output=True, text=True, check=True)
     except (FileNotFoundError, subprocess.CalledProcessError) as ex:
         raise BuildError(f"unable to run {prog}") from ex
-    print(f"detected {prog}:\n{textwrap.indent(result.stdout, '.. ')}", end="")
+    logger.info(f"detected {prog}:\n{result.stdout.strip()}")
 
 
 def _resolve_source(
@@ -131,6 +151,7 @@ def _resolve_source(
 
     Raises ``BuildError`` on download or validation failure.
     """
+    logger = logging.getLogger(__name__)
 
     if (
         archive_directory is not None
@@ -140,13 +161,13 @@ def _resolve_source(
             )
         ).exists()
     ):
-        print(f"Using cached source '{archive_path}'")
+        logger.info(f"Using cached source '{archive_path}'")
 
         sixs_archive = archive_path.read_bytes()
 
     else:
         # No cached source - download fresh copy.
-        print(f"Downloading 6S archive from '{target.archive_url}'")
+        logger.info(f"Downloading 6S archive from '{target.archive_url}'")
 
         response: http.client.HTTPResponse = urllib.request.urlopen(target.archive_url)
         if response.status != http.HTTPStatus.OK.value:
@@ -156,20 +177,24 @@ def _resolve_source(
             )
         sixs_archive = response.read()
 
+    logger.debug("validating archive against expected hash %r", target.archive_sha256)
     digest = hashlib.sha256(sixs_archive).hexdigest()
     if digest != target.archive_sha256:
-        raise RuntimeError(
+        raise BuildError(
             f"6S archive hash validation failed. "
             f"Expected SHA256={target.archive_sha256}, got SHA256={digest}"
         )
 
-    print(f"Extracting source to '{directory}'")
+    logger.info(f"Extracting source...")
+    logger.debug("Extracting %d byte payload to '%s'", len(sixs_archive), directory)
     buffer = io.BytesIO(sixs_archive)
     tar_file = tarfile.open(fileobj=buffer, mode="r:")
     tar_file.extractall(directory)
 
 
 def _test_sixs(binary: pathlib.Path, example_dir: pathlib.Path) -> None:
+    logger = logging.getLogger(__name__)
+
     for example_in in example_dir.glob("Example_In_*.txt"):
         # Corresponding expected output file.
         example_out = example_in.parent.joinpath(example_in.name.replace("In", "Out"))
@@ -184,12 +209,10 @@ def _test_sixs(binary: pathlib.Path, example_dir: pathlib.Path) -> None:
                 cwd=example_dir,
             )
         except subprocess.CalledProcessError as ex:
-            stdout = textwrap.indent(ex.stdout, ".. ")
-            stderr = textwrap.indent(ex.stderr, ".. ")
             raise BuildError(
                 f"running example {example_in.name} failed\n"
-                f"stdout:\n{stdout}\n"
-                f"stderr:\n{stderr}"
+                f"stdout:\n{ex.stdout}\n"
+                f"stderr:\n{ex.stderr}"
             ) from ex
 
         # Compare actual output with expected output.
@@ -206,7 +229,7 @@ def _test_sixs(binary: pathlib.Path, example_dir: pathlib.Path) -> None:
             )
             diff_text = "\n".join(diff)
 
-            print(
+            logger.warning(
                 f"WARNING: incorrect output for test {example_in.name}\n"
                 f"diff:\n{diff_text}"
                 # f"expected:\n{textwrap.indent(expected, '.. ')}\n"
@@ -222,27 +245,32 @@ def _install(binary: pathlib.Path, target: pathlib.Path) -> None:
 
 def build(target: SixSTarget, build_dir: pathlib.Path, config: BuildConfig) -> None:
     """Run build in the given directory."""
+    logger = logging.getLogger(__name__)
+    logger.debug(
+        f"building %s in '{build_dir}' with config %r", target.target_name, config
+    )
+
     binary_dest = PACKAGE_ROOT.joinpath(target.target_name)
+    logger.debug(f"destination for %s is '%s'", target.target_name, binary_dest)
 
     if binary_dest.is_file():
         # Binary already exists in package. Skip rebuilding.
-        print(f"target {binary_dest} already exists - skipping build")
+        logger.info(f"target {binary_dest} already exists - skipping build")
         return
 
-    print(f"Resolving 6S source...")
+    logger.info(f"Resolving 6S source...")
     _resolve_source(target, build_dir, config.archive_dir)
-    # Print extracted files for debugging.
-    # for p in sorted(build_dir.glob("**/*")):
-    #     print(f"file: {p}")
+    logger.debug("source contents: %r", sorted(build_dir.glob("**/*")))
 
     # Make 6S executable.
-    print("Building...")
+    logger.info("Building...")
     try:
         # If 6S source contains a directory beginning with 6S, build from that directory.
         (src_dir,) = build_dir.glob("6S*")
     except (FileNotFoundError, ValueError):
         # No 6S* subdirectory. Build from source root.
         src_dir = build_dir
+    logger.debug("resolved source directory to be '%s'", src_dir)
 
     if not src_dir.joinpath("Makefile").exists():
         raise BuildError(f"could not find Makefile in source directory '{src_dir}'")
@@ -251,58 +279,90 @@ def build(target: SixSTarget, build_dir: pathlib.Path, config: BuildConfig) -> N
         f"FC=gfortran -std=legacy -ffixed-line-length-none"
         f" -ffpe-summary=none {'-static-libgfortran' if config.static_libgfortran else ''} $(FFLAGS)"
     )
+    make_command = ["make", "-j", "sixs", fc_override]
+    logger.debug("running make command %r", make_command)
     try:
-        subprocess.run(
-            [
-                "make",
-                "-j",
-                "sixs",
-                fc_override,
-            ],
+        result = subprocess.run(
+            make_command,
             cwd=src_dir,
             capture_output=True,
             check=True,
             text=True,
         )
     except subprocess.CalledProcessError as ex:
-        stdout = textwrap.indent(ex.stdout, ".. ")
-        stderr = textwrap.indent(ex.stderr, ".. ")
         raise BuildError(
-            f"failed to compile 6s.\nstdout:\n{stdout}\nstderr:\n{stderr}"
+            f"make exited with non-zero exit status {ex.returncode}\nstdout:\n{ex.stdout}\nstderr:\n{ex.stderr}",
         ) from ex
+    logger.debug("make output\nstdout:\n%s\nstderr:\n%s", result.stdout, result.stderr)
 
     # Path to built binary.
     sixs_binary = src_dir.joinpath(target.target_name)
+    if not sixs_binary.is_file():
+        raise BuildError(f"make did not produce binary at '{sixs_binary}'")
+    logger.debug(
+        "output binary %s has size %d", sixs_binary, sixs_binary.stat().st_size
+    )
 
     # Validate built binary against example suite.
     example_dir = build_dir.joinpath("Examples")
     if example_dir.exists():
-        print("Testing...")
+        logger.info("Testing...")
         _test_sixs(sixs_binary, build_dir.joinpath("Examples"))
     else:
-        print("Skipping tests - no examples found")
+        logger.info("Skipping tests - no examples found")
 
     # Install 6S executable into package source.
-    print("Installing...")
+    logger.info("Installing...")
+    logger.debug("copying '%s' to '%s'", sixs_binary, binary_dest)
     _install(sixs_binary, binary_dest)
+
+
+def _setup_logging(log_file: pathlib.Path | None) -> None:
+    logger = logging.getLogger(__name__)
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(
+        WrapFormatter("{levelname}: {message}", style="{", validate=True)
+    )
+    console_handler.setLevel(logging.INFO)
+    logger.addHandler(console_handler)
+
+    if log_file is not None:
+        file_handler = logging.FileHandler(log_file, mode="a")
+        file_handler.setFormatter(
+            WrapFormatter(
+                "[{asctime}] {levelname:7s} {funcName}:{lineno} {message}",
+                style="{",
+                datefmt="%Y-%m-%d %H:%M:%S",
+                validate=True,
+            )
+        )
+        file_handler.setLevel(logging.DEBUG)
+        logger.addHandler(file_handler)
+
+    logger.setLevel(logging.DEBUG)
 
 
 def main() -> None:
     """Build script entrypoint."""
+    config = BuildConfig.from_env()
+
+    _setup_logging(config.log_file)
+    logger = logging.getLogger(__name__)
+    logger.info(f"Building from '%s' with config %r", pathlib.Path.cwd(), config)
 
     # Check system dependencies
-    print("Checking system dependencies...")
+    logger.info("Checking system dependencies...")
     _assert_detect_command(["make", "--version"])
     _assert_detect_command(["gfortran", "--version"])
 
-    config = BuildConfig.from_env()
-    print(f"Using {config}")
-
     # Build each 6S target.
     for target in TARGETS:
-        print(f"target: {target.target_name}")
+        logger.info(f"Processing target {target.target_name}")
         with tempfile.TemporaryDirectory() as build_dir:
             build(target, pathlib.Path(build_dir), config)
+
+    logger.info("Build finished normally")
 
 
 if __name__ == "__main__":
