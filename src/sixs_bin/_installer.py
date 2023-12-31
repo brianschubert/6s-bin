77b5121ab066c0bc3426abbf3c4a017dd945de96
Brian Schubert <schubert.b@northeastern.edu>
2023-12-31 12:26:57 -0500

    Add initial implementation of self-installing console scripts

---
+++ src/sixs_bin/_installer.py
@@ -0,0 +1,74 @@
+"""
+Entrypoints for console scripts that replace themselves with 6S binaries when first run.
+"""
+
+import os
+import pathlib
+import subprocess
+import sys
+
+import sixs_bin
+
+
+def _main_path() -> pathlib.Path:
+    """Retrieve the absolute path to the current ``__main__`` module."""
+    main = sys.modules["__main__"]
+    if not hasattr(main, "__file__") or main.__file__ is None:
+        raise RuntimeError(
+            f"__main__ module {main} is missing __file__ - cannot retrieve path to __main__"
+        )
+    return pathlib.Path(main.__file__).resolve()
+
+
+def _install(src: pathlib.Path, target: pathlib.Path) -> None:
+    """
+    Install ``src`` to ``target``.
+
+    Attempts to hardlink ``target`` to ``src`` if possible. Otherwise, falls
+    back to writing the contents of ``src`` to ``target`` inplace.
+    """
+    temp_target = target.with_name(f"~{target.name}")
+
+    try:
+        os.link(src, temp_target)
+    except OSError:
+        # Failed to create new hardlink in bin directory.
+        # Fall back to simply writing the contents of the source file inplace.
+        target.write_bytes(src.read_bytes())
+        return
+
+    # Successfully created new hardlink in the bin directory.
+    # Remove the old script, then move the hardlink to the old script's location.
+    target.unlink()
+    temp_target.rename(target)
+
+
+def _inplace_install_11() -> None:
+    """Replace the current ``__main__`` module with a 6S V1.1 binary."""
+    _run_inplace_install(sixs_bin.get_path("1.1"), _main_path())
+
+
+def _inplace_install_21() -> None:
+    """Replace the current ``__main__`` module with a 6S V2.1 binary."""
+    _run_inplace_install(sixs_bin.get_path("2.1"), _main_path())
+
+
+def _run_inplace_install(src: pathlib.Path, target: pathlib.Path) -> None:
+    print(
+        f"Starting first-run installation for {target.name}.\n"
+        f"This will install the 6S binary '{src}' to '{target}'.\n...",
+        file=sys.stderr,
+        flush=True,
+    )
+    try:
+        _install(src, target)
+    except OSError:
+        print(
+            "error: first-run installation failed! See accompanying exception traceback."
+        )
+        raise
+
+    print(
+        "First-run installation succeeded! Launching 6S in a subprocess. All future runs will execute 6S directly."
+    )
+    subprocess.run([target, *sys.argv[1:]])
